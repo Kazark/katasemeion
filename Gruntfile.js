8291ddd5c4bea0949f3ef75a885f0b879fad306d
Kazark <kazark@zoho.com>
2014-02-14 23:29:39 -0500

    Bootstrapped the project for Grunt & Jasmine

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+    var mainOutputScript = 'build/katasemeion.js';
+    var jasmineOutputFile = 'build/test-results.html';
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: ''
+            },
+            app: {
+                dest: mainOutputScript,
+                src: [ 'src/*.js' ]
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['src/*.js*', 'unittests/*.spec.js'],
+                tasks: ['unittests']
+            }
+        },
+        jshint: {
+            all: {
+                files: {
+                    src: ['build/app.js']
+                }
+            }
+        },
+        jasmine: {
+            all: {
+                src: mainOutputScript,
+                options: {
+                    specs: 'unittests/*.spec.js',
+                    outfile: jasmineOutputFile,
+                    keepRunner: true
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    grunt.registerTask('unittests', ['concat', 'jshint', 'jasmine:all']);
+    grunt.registerTask('default', ['unittests']);
+    grunt.registerTask('dev', ['default', 'watch']);
+};
---
+++ unittests/main.spec.js
@@ -0,0 +1,5 @@
+describe('Κατασημεῖον top-level module', function() {
+    it('should exist', function() {
+        expect(katasemeion).toBeTruthy();
+    });
+});
