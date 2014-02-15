c928beefb551fd7a5b1abafd1cda20309391ccd2
Kazark <kazark@zoho.com>
2014-02-15 14:32:18 -0500

    Implemented underscore tokenizer
    Refactored tokenizer factory code to create functional, fluent
    interface.

--- src/lexer.js
+++ src/lexer.js
@@ -1,27 +1,44 @@
 katasemeion.lexer = (function(tokens) {
     var self = this;
 
-    var makeSingleOrDoubleCharacterTokenizer = function(character, singleToken, doubleToken) {
-        return function(sourceStream) {
-            if (sourceStream.current === character) {
-                sourceStream.advanceCursor();
-                if (sourceStream.current === character) {
-                    sourceStream.advanceCursor();
-                    return doubleToken();
-                }
-                return singleToken();
+    var ifCharIs = function(character) {
+        return {
+            thenReturn: function(thenToken) {
+                var _elseToken;
+                var makeTokenizer = function(sourceStream) {
+                    if (sourceStream.current === character) {
+                        sourceStream.advanceCursor();
+                        return thenToken();
+                    }
+                    return _elseToken ? _elseToken() : null;
+                };
+                makeTokenizer.elseReturn = function(elseToken) {
+                    _elseToken = elseToken;
+                    return makeTokenizer;
+                };
+                makeTokenizer.elifDoubleReturn = function(doubledToken) {
+                    return function(sourceStream) {
+                        return ifCharIs(character).
+                               thenReturn(function() {
+                                   return ifCharIs(character).
+                                          thenReturn(doubledToken).
+                                          elseReturn(thenToken)(sourceStream);
+                               })(sourceStream);
+                    };
+                };
+                return makeTokenizer;
             }
-            return null;
         };
     };
 
-    self.tokenizeOpenBracket = makeSingleOrDoubleCharacterTokenizer(
-        '[', tokens.OpenBracket, tokens.DoubleOpenBracket
-    );
-
-    self.tokenizeCloseBracket = makeSingleOrDoubleCharacterTokenizer(
-        ']', tokens.CloseBracket, tokens.DoubleCloseBracket
-    );
+    self.tokenizeOpenBracket = ifCharIs('[').
+                               thenReturn(tokens.OpenBracket).
+                               elifDoubleReturn(tokens.DoubleOpenBracket);
+    self.tokenizeCloseBracket = ifCharIs(']').
+                                thenReturn(tokens.CloseBracket).
+                                elifDoubleReturn(tokens.DoubleCloseBracket);
+    self.tokenizeUnderscore = ifCharIs('_').
+                              thenReturn(tokens.Underscore);
 
     return self;
 })(katasemeion.tokens);
--- unittests/lexer.spec.js
+++ unittests/lexer.spec.js
@@ -10,9 +10,11 @@ describe('Κατασημεῖον lexer', function() {
     describe('open bracket tokenizer', function() {
         var tokenize =  lexer.tokenizeOpenBracket;
 
-        it('should do nothing if there is no bracket in the stream', function() {
-            var token = tokenize(sourceStream('asdf'));
+        it('should do nothing if there is no bracket at the beginning of the stream', function() {
+            var characters = sourceStream('a[df');
+            var token = tokenize(characters);
             expect(token).toBeFalsy();
+            expect(characters.current).toBe('a');
         });
 
         it('should return an open bracket token when stream cursor points to an open bracket', function() {
@@ -31,7 +33,7 @@ describe('Κατασημεῖον lexer', function() {
             expect(token.is(tokens.DoubleOpenBracket)).toBe(true);
         });
 
-        it('should advance the cursor when it has parsed a double open bracket token', function() {
+        it('should advance the cursor when it has parsed a token', function() {
             var characters = sourceStream('[[df');
             var token = tokenize(characters);
             expect(characters.current).toBe('d');
@@ -41,17 +43,19 @@ describe('Κατασημεῖον lexer', function() {
     describe('close bracket tokenizer', function() {
         var tokenize =  lexer.tokenizeCloseBracket;
 
-        it('should do nothing if there is no bracket in the stream', function() {
-            var token = tokenize(sourceStream('asdf'));
+        it('should do nothing if there is no brackeat the beginning of the stream', function() {
+            var characters = sourceStream('a]df');
+            var token = tokenize(characters);
             expect(token).toBeFalsy();
+            expect(characters.current).toBe('a');
         });
 
-        it('should return an close bracket token when stream cursor points to an close bracket', function() {
+        it('should return a close bracket token when stream cursor points to a close bracket', function() {
             var token = tokenize(sourceStream(']sdf'));
             expect(token.is(tokens.CloseBracket)).toBe(true);
         });
 
-        it('should advance the cursor when it has parsed an close bracket token', function() {
+        it('should advance the cursor when it has parsed a close bracket token', function() {
             var characters = sourceStream(']sdf');
             var token = tokenize(characters);
             expect(characters.current).toBe('s');
@@ -62,10 +66,32 @@ describe('Κατασημεῖον lexer', function() {
             expect(token.is(tokens.DoubleCloseBracket)).toBe(true);
         });
 
-        it('should advance the cursor when it has parsed a double close bracket token', function() {
+        it('should advance the cursor when it has parsed a token', function() {
             var characters = sourceStream(']]df');
             var token = tokenize(characters);
             expect(characters.current).toBe('d');
+        });
+    });
+
+    describe('underscore tokenizer', function() {
+        var tokenize =  lexer.tokenizeUnderscore;
+
+        it('should do nothing if there is no underscore at the beginning of the stream', function() {
+            var characters = sourceStream('a_df');
+            var token = tokenize(characters);
+            expect(token).toBeFalsy();
+            expect(characters.current).toBe('a');
+        });
+
+        it('should return an underscore token when stream cursor points to an underscore', function() {
+            var token = tokenize(sourceStream('_sdf'));
+            expect(token.is(tokens.Underscore)).toBe(true);
+        });
+
+        it('should advance the cursor when it has parsed a token', function() {
+            var characters = sourceStream('_sdf');
+            var token = tokenize(characters);
+            expect(characters.current).toBe('s');
         });
     });
 });
