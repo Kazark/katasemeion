57c46b0ef9a974cc534fe244e4a71dcfbd955745
Kazark <kazark@zoho.com>
2014-02-21 13:05:40 -0500

    Reduced indentation in tokenizer factory function

--- src/lexer.js
+++ src/lexer.js
@@ -2,40 +2,41 @@ katasemeion.lexer = (function(tokens) {
     var self = this;
 
     var ifCharIs = function(character) {
-        return {
-            thenReturn: function(thenToken) {
-                var _elseToken = function() { return null; };
-                var tokenize = function(sourceStream) {
-                    if (sourceStream.current === character) {
-                        sourceStream.advanceCursor();
-                        return thenToken();
-                    }
-                    return _elseToken();
-                };
-                tokenize.elseReturn = function(elseToken) {
-                    _elseToken = elseToken;
-                    return tokenize;
-                };
-                tokenize.unlessFollowedBy = function(alternateChar) {
-                    return {
-                        thenReturn: function(alternateToken) {
-                            return function(sourceStream) {
-                                return ifCharIs(character).
-                                       thenReturn(function() {
-                                           return ifCharIs(alternateChar).
-                                                  thenReturn(alternateToken).
-                                                  elseReturn(thenToken)(sourceStream);
-                                       })(sourceStream);
-                            };
-                        }
-                    };
-                };
-                tokenize.elifDoubledReturn = function(doubledToken) {
-                    return tokenize.unlessFollowedBy(character)
-                                   .thenReturn(doubledToken);
-                };
+        var thenReturn = function(thenToken) {
+            var _elseToken = function() { return null; };
+            var tokenize = function(sourceStream) {
+                if (sourceStream.current === character) {
+                    sourceStream.advanceCursor();
+                    return thenToken();
+                }
+                return _elseToken();
+            };
+            tokenize.elseReturn = function(elseToken) {
+                _elseToken = elseToken;
                 return tokenize;
-            }
+            };
+            tokenize.unlessFollowedBy = function(alternateChar) {
+                return {
+                    thenReturn: function(alternateToken) {
+                        return function(sourceStream) {
+                            return ifCharIs(character).
+                                   thenReturn(function() {
+                                       return ifCharIs(alternateChar).
+                                              thenReturn(alternateToken).
+                                              elseReturn(thenToken)(sourceStream);
+                                   })(sourceStream);
+                        };
+                    }
+                };
+            };
+            tokenize.elifDoubledReturn = function(doubledToken) {
+                return tokenize.unlessFollowedBy(character)
+                               .thenReturn(doubledToken);
+            };
+            return tokenize;
+        };
+        return {
+            thenReturn: thenReturn
         };
     };
 
