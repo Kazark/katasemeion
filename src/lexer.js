d78c9607e45a37d43358d11c491c4db70f86e066
Kazark <kazark@zoho.com>
2014-03-08 14:32:29 -0500

    Lexer: pass parsed tokens to a callback function

--- src/lexer.js
+++ src/lexer.js
@@ -1,13 +1,16 @@
-katasemeion.make.lexer = function(tokenizers) {
+katasemeion.make.lexer = function(tokenizers, callback) {
     var self = {};
     self.tokenizers = tokenizers;
     self.lex = function(sourceStream) {
         tokenizers.all.forEach(function(tokenize) {
-            tokenize(sourceStream);
+            var token = tokenize(sourceStream);
+            if (token) {
+                callback(token);
+            }
         });
     };
     return self;
 };
 
-katasemeion.lexer = katasemeion.make.lexer(katasemeion.tokenizers);
+katasemeion.lexer = katasemeion.make.lexer(katasemeion.tokenizers, function() {});
 
--- unittests/lexer.spec.js
+++ unittests/lexer.spec.js
@@ -1,21 +1,45 @@
 describe('Κατασημεῖον lexer', function() {
+    var tokens = katasemeion.tokens;
+
     it('should exist', function() {
         expect(katasemeion.lexer).toBeTruthy();
     });
 
     describe('lex', function() {
-        var fakeTokenizers = [
-            jasmine.createSpy('tokenizer1'),
-            jasmine.createSpy('tokenizer2')
-        ];
-        var lexer = katasemeion.make.lexer({ all : fakeTokenizers });
-        var stream = katasemeion.sourceStream("a");
+        var fakeTokenizers,
+            tokenOutputCallback,
+            lexer,
+            stream;
+
+        beforeEach(function() {
+            fakeTokenizers = [
+                jasmine.createSpy('tokenizer1'),
+                jasmine.createSpy('tokenizer2')
+            ];
+            tokenOutputCallback = jasmine.createSpy('tokenOutputCallback');
+            lexer = katasemeion.make.lexer({ all : fakeTokenizers }, tokenOutputCallback);
+            stream = katasemeion.sourceStream("a");
+        });
 
         it('should invoke each of the tokenizers in turn', function() {
             lexer.lex(stream);
 
             expect(fakeTokenizers[0]).toHaveBeenCalledWith(stream);
             expect(fakeTokenizers[1]).toHaveBeenCalledWith(stream);
+        });
+
+        it('should not pass return value of tokenizers to the callback function if it is null', function() {
+            lexer.lex(stream);
+
+            expect(tokenOutputCallback).not.toHaveBeenCalled();
+        });
+
+        it('should pass parsed tokens to the callback function', function() {
+            fakeTokenizers.push(function() { return tokens.Hash; });
+
+            lexer.lex(stream);
+
+            expect(tokenOutputCallback).toHaveBeenCalledWith(tokens.Hash);
         });
     });
 });
