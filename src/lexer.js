846ce190361eaad9f72e5c54376d71fc0b5c4bc2
Kazark <kazark@zoho.com>
2014-03-08 13:21:23 -0500

    Sketched out skeletal lexer

--- src/lexer.js
+++ src/lexer.js
@@ -1,4 +1,10 @@
 katasemeion.lexer = (function(tokenizers) {
     var self = {};
+    self.tokenizers = tokenizers;
+    self.lex = function() {
+        tokenizers.all.forEach(function(tokenize) {
+            tokenize();
+        });
+    };
     return self;
 })(katasemeion.tokenizers);
--- unittests/lexer.spec.js
+++ unittests/lexer.spec.js
@@ -1,7 +1,30 @@
 describe('Κατασημεῖον lexer', function() {
     var lexer = katasemeion.lexer;
+    var tokenizers = katasemeion.tokenizers;
 
     it('should exist', function() {
         expect(lexer).toBeTruthy();
+    });
+
+    describe('lex', function() {
+        var all;
+        beforeEach(function() { /* Sure would be nice to have a beforeAll instead for part of this... */
+            all = tokenizers.all;
+            tokenizers.all = [
+                jasmine.createSpy('tokenizer1'),
+                jasmine.createSpy('tokenizer2')
+            ];
+        });
+
+        it('should invoke each of the tokenizers in turn', function() {
+            lexer.lex();
+
+            expect(tokenizers.all[0]).toHaveBeenCalled();
+            expect(tokenizers.all[1]).toHaveBeenCalled();
+        });
+        
+        afterEach(function() { /* Sure would be nice to have an afterAll instead... */
+            tokenizers.all = all;
+        });
     });
 });
